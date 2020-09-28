d551b8b286956ce58589ae3cab40241dbc891610
AdamHtooLwin <adamhtoolwin@gmail.com>
2020-09-28 13:42:30 +0700

    Remove all transforms.

--- datasets/datasets.py
+++ datasets/datasets.py
@@ -13,15 +13,19 @@ from albumentations.pytorch import ToTensorV2 as ToTensor
 def get_train_augmentations(image_size: int = 224, mean: tuple = (0, 0, 0), std: tuple = (1, 1, 1)):
     return A.Compose(
         [
+            # A.RandomBrightnessContrast(brightness_limit=32, contrast_limit=(0.5, 1.5)),
+            # A.HueSaturationValue(hue_shift_limit=18, sat_shift_limit=(1, 2)),
             # A.CoarseDropout(20),
-            # A.Rotate(30),
+            A.Rotate(10),
+
             A.Resize(image_size, image_size),
             # A.RandomCrop(image_size, image_size, p=0.5),
+
             A.LongestMaxSize(image_size),
             A.Normalize(mean=mean, std=std),
             A.HorizontalFlip(),
             A.PadIfNeeded(image_size, image_size, 0),
-            A.Transpose(),
+            # A.Transpose(),
             ToTensor(),
         ]
     )

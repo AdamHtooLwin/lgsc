f6c675f3c5b72418223144604b5d5e71eda4f433
AdamHtooLwin <adamhtoolwin@gmail.com>
2020-12-07 10:23:40 +0700

    Finalized video decoder

--- datasets/oulu-npu/video_decoder.py
+++ datasets/oulu-npu/video_decoder.py
@@ -5,6 +5,8 @@ from tqdm import tqdm
 import cv2
 import argparse
 import pandas as pd
+from facenet_pytorch import MTCNN
+from PIL import Image
 
 
 def make_folder(folder):
@@ -45,6 +47,7 @@ def delete():
 if __name__ == "__main__":
     default_labels_file = "/root/datasets/oulu/labels.txt"
     default_output_csv = "/root/datasets/oulu/labels.csv"
+    default_device = 'cuda:0'
 
     parser = argparse.ArgumentParser(
             description='Extract frames from OULU videos.',
@@ -53,9 +56,12 @@ if __name__ == "__main__":
 
     parser.add_argument('videos_folder', type=str, help='Absolute path to folder to process videos.')
     parser.add_argument('-t', '--file_type', type=str, help='File type of video.', default="avi")
-    parser.add_argument('-f', '--frames', type=int, help='Number of frames to extract.', default=4)
+    parser.add_argument('-f', '--frames', type=int, help='Number of frames to extract.', default=5)
     parser.add_argument('-o', '--output_csv', type=str, help='File to output CSV label file.', default=default_output_csv)
     parser.add_argument('-l', '--labels_file', type=str, help='The file to refer to for the labels.', default=default_labels_file)
+    parser.add_argument('-d', '--device', type=str, help='The device to run MTCNN face cropper.', default=default_device)
+    parser.add_argument('-s', '--output_size', type=int, help='Output size of cropped images.', default=224)
+    parser.add_argument('-de', '--debug', type=bool, help='Debug mode.', default=False)
 
     args = parser.parse_args()
 
@@ -70,6 +76,8 @@ if __name__ == "__main__":
         'target': []
     }
 
+    face_detector = MTCNN(image_size=args.output_size, device=args.device)
+
     pbar = tqdm(video_files)
     for video_file in pbar:
         video_folder_name = video_file.split("/")[-1].split(".")[0]
@@ -78,8 +86,10 @@ if __name__ == "__main__":
         label = labels_df.loc[labels_df['type'] == int(video_type)].iloc[0].target
         pbar.set_description("Processing video %s " % video_folder_name)
         video_folder = img_folder + video_folder_name
+        uncropped_folder = video_folder + "/uncropped"
 
         make_folder(video_folder)
+        make_folder(uncropped_folder)
 
         vidcap = cv2.VideoCapture(video_file)
 
@@ -91,16 +101,25 @@ if __name__ == "__main__":
         while success:
             if count == 3 or count % increment == 0:
                 file_name = video_folder + "/%d.jpg" % count
+                uncropped_path = uncropped_folder + "/%d.jpg" % count
 
                 data['path'].append(file_name)
                 data['target'].append(label)
-                cv2.imwrite(file_name, image)  # save frame as JPEG file
+
+                cv2.imwrite(uncropped_path, image)  # save uncropped frame
+
+                image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+                image_pil = Image.fromarray(image)
+                cropped_image = face_detector(image_pil, save_path=file_name)   # save cropped frame
 
             success, image = vidcap.read()
             # print('Read a new frame: ', success)
             count += 1
 
         vidcap.release()
+
+        if args.debug and video_type == '5':
+            break
 
     df = pd.DataFrame(data)
     df.to_csv(args.output_csv, index=False)
